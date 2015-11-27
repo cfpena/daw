139ddb1c5c96e3a067603c016205afb1379347ba
jjjohncedeno <jjjohncedeno@gmail.com>
2015-11-27 01:06:26 -0500

    Add Login of WebPage. Add template of login on the page. Also is the design of template.

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  jQuery.backstretch([
+        "img/img1.jpg"
+      , "img/img2.jpg"
+      , "img/img3.jpg"
+      , "img/img4.jpg"
+      ], {duration: 4000, fade: 1000}
+  );
+});
