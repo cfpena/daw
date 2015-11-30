8198b3716fda1dcf520726dc75e029e850a72733
cristian <cfpena@espol.edu.ec>
2015-11-30 00:09:33 -0500

    panel, drag drop, upload

--- js/example.js
+++ js/example.js
@@ -4,19 +4,19 @@ jsPlumb.ready(function() {
 
   $('#container').dblclick(function(e) {
     var newState = $('<div>').attr('id', 'state' + i).addClass('item');
-    
+
     var title = $('<div>').addClass('title').text('State ' + i);
     var connect = $('<div>').addClass('connect');
-    
+
     newState.css({
       'top': e.pageY,
-      'left': e.pageX
+      'left': e.pageX,
     });
-    
+
     jsPlumb.makeTarget(newState, {
       anchor: 'Continuous'
     });
-    
+
     jsPlumb.draggable(newState, {
       containment: 'parent'
     });
@@ -31,14 +31,13 @@ jsPlumb.ready(function() {
       $(this).remove();
       e.stopPropagation();
     });
-    
+
     newState.append(title);
     newState.append(connect);
-    
+
     $('#container').append(newState);
-    
-    i++;    
-  });  
+
+    i++;
+  });
 
 });
-
---
+++ js/svg.js
@@ -0,0 +1,68 @@
+$(function() {
+
+        paper = new Raphael(document.getElementById('drawContainer'), 1000 , 1000);
+        $("#entidadButton").click(drawEntidad);
+        $("#cargarButton").click(uploadSvg);
+});
+function drawEntidad() {
+
+     c = paper.path("M 0 0 L 0 100 L 100 100 L 100 0 L 0 0 M 0 30 L 100 30 Z").attr({
+             fill: "white",
+             stroke: "black",
+             opacity: 1,
+             cursor: "move",
+
+         });
+    c.drag(move,start,up);
+}
+function prepareUpload(event)
+{
+  files = event.target.files;
+}
+
+
+
+
+
+
+function uploadSvg(){
+  var input = ('<input type="file" id="file" style="display:none" />');
+  $("#drawContainer").append(input);
+  $("#file").change(function ()
+   {
+     var fileSelect = $("#file");
+     var formData = new FormData();
+     var file = fileSelect[0].files[0];
+     formData.append("svg",file);
+     var xhr = new XMLHttpRequest();
+     xhr.open('POST', 'workspace.html', true);
+     xhr.onload = function () {
+      if (xhr.status === 200) alert("sucess!");
+      else alert('An error occurred!');
+    };
+      xhr.send(formData);
+   });
+
+
+
+  $("#file").click();
+
+
+}
+
+
+
+
+var start = function () {
+this.odx = 0;
+this.ody = 0;
+this.animate({"stroke-opacity": 0.2}, 500);
+},
+move = function (dx, dy) {
+this.translate(dx - this.odx, dy - this.ody);
+this.odx = dx;
+this.ody = dy;
+},
+up = function () {
+this.animate({"stroke-opacity": 1}, 500);
+};
